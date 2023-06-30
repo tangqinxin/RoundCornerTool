ee4e6f93a16ab47fdf660e5024f305584b788b33
tangqinxin <448373336@qq.com>
2023-07-01 02:07:13 +0800

    complete the roundcorner tool version1.0

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFilter
+import cv2
+import numpy as np
+
+# Step1: input parameters
 width = 1080
 height = 2504
 radius = 100
+
+# Step2: create a roundCorner resource
 # Create a new image with black background
 img = Image.new('RGB', (width, height), color = 'black') # 底板为纯黑图
 
@@ -27,6 +33,33 @@ img.putalpha(mask)
 # Save the image as PNG format
 img.save('image.png')
 
+# Step3: blur the resource to smooth the edge of round corner
+# this step can be skip if not necessary
+
+# class MY_FILTER(ImageFilter.BuiltinFilter):
+#     name = "my_filter"
+#     # fmt: off
+#     filterargs = (3, 3), 5, 0, (
+#         1, 1, 1,
+#         1, 5, 1,
+#         1, 1, 1,
+#     )
+# blurred_image = img.filter(MY_FILTER)
+
+blurred_image = img.filter(ImageFilter.SMOOTH)
+blurred_image = blurred_image.filter(ImageFilter.SMOOTH) # if one blur is not enough, 2 blur can be done
+
+# blurred_image.save('blur.png')
+
+# Step4: use opencv to deal with the image
+# 使用opencv进行处理边缘
+img3 = cv2.imread('pic_whiteBackGround.png')  # pic_whiteBackGround.png should be white background
+img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+
+middleNumber = 7  # the middle number of the 3x3 smooth kernel
+kernel = np.array([[1, 1, 1], [1, middleNumber, 1], [1, 1, 1]]) / (8 + middleNumber)
+img3 = cv2.filter2D(img3, -1, kernel)
+img3 = cv2.copyMakeBorder(img3, 0, 0, 0, 0, cv2.BORDER_CONSTANT)
+cv2.imwrite('output.png', img3)
+
 print('width =', width, 'height =', height, 'radius =', radius)
-
-
